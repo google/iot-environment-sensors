34ece839e73afe0aa6a91994cf8d90f4b40878bb
Lucas Cooper <bobobo@google.com>
2020-07-16 12:43:13 +0200

    Update to go 1.13

--- cloud_function/function.go
+++ cloud_function/function.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package gastranslator
+package cloud_function
 
 import (
 	"cloud.google.com/go/bigquery"
